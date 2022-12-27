24e21a73e6f252436296374a25989a7087184013
T-Troll <rk@whowashere.ru>
2022-12-27 12:31:48 +0700

    Release preparation

--- alienfx-cli/alienfx-cli.cpp
+++ alienfx-cli/alienfx-cli.cpp
@@ -127,7 +127,7 @@ int main(int argc, char* argv[])
 	int devType = -1;
 	UINT sleepy = 0;
 
-	printf("alienfx-cli v7.7.2\n");
+	printf("alienfx-cli v7.10.1\n");
 	if (argc < 2)
 	{
 		printUsage();
