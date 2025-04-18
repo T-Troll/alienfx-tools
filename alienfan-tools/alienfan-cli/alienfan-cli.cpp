a6305db087b3d725edb2c407cfc59e546e9e4e3d
T-Troll <rk@wayphone.net>
2025-04-18 09:56:24 +0300

    Release preparation

--- alienfan-tools/alienfan-cli/alienfan-cli.cpp
+++ alienfan-tools/alienfan-cli/alienfan-cli.cpp
@@ -147,7 +147,7 @@ setbrightness=<brightness>\tSet lights brightness\n");
 
 int main(int argc, char* argv[])
 {
-    printf("AlienFan-CLI v9.1.4.2\n");
+    printf("AlienFan-CLI v9.1.4.3\n");
 #ifndef NOLIGHTS
     AlienFan_SDK::Lights* lights = NULL;
 #endif
