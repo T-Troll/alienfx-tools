7dd548d32fe11f9b712e346b36001748b8c0925a
StoolMonster <lfw93814@gmail.com>
2025-05-13 18:26:31 +0800

    add a TccBox
    Add a box to show and edit tcc manully.

--- alienfx-gui/resource.h
+++ alienfx-gui/resource.h
@@ -218,6 +218,7 @@
 #define IDC_OCENABLE                    1215
 #define IDC_DISKSENSORS                 1216
 #define IDC_LIGHTACTION                 1217
+#define IDC_EDIT_TCC                    1218
 #define IDC_BUTREMRGH                   1228
 #define IDC_BUTREMBOT                   1229
 #define IDC_BUTADDTOP                   1230
@@ -245,7 +246,7 @@
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        167
 #define _APS_NEXT_COMMAND_VALUE         32814
-#define _APS_NEXT_CONTROL_VALUE         1218
+#define _APS_NEXT_CONTROL_VALUE         1219
 #define _APS_NEXT_SYMED_VALUE           113
 #endif
 #endif
