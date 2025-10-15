f90ba175a35c4c5abfcc451a554feb8cab402a3e
roman.kozlov <roman.kozlov@auriga.ru>
2025-10-15 09:19:26 +0300

    Area 51 alt profile

--- alienfx-cli/alienfx-cli.cpp
+++ alienfx-cli/alienfx-cli.cpp
@@ -108,7 +108,7 @@ vector<AlienFX_SDK::Afx_action> ParseActions(vector<ARG>* args, int startPos) {
 
 int main(int argc, char* argv[])
 {
-	printf("alienfx-cli v9.3.3.1\n");
+	printf("alienfx-cli v9.3.4\n");
 	if (argc < 2)
 	{
 		printUsage();
